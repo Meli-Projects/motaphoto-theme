01fb6fe7a509b9793713a7fd9f9fe772da4ea46c
Meli-Projets <melisande77320@gmail.com>
2026-05-20 14:48:08 +0200

    Added footer structure and CSS

--- footer.php
+++ footer.php
@@ -1,7 +1,22 @@
-<footer>
-    <p>Footer</p>
+<footer class="footer-site">
+
+    <div class="footer-container">
+
+        <a href="<?php echo get_permalink( get_page_by_path('mentions-legales') ); ?>">
+            Mentions légales
+        </a>
+
+        <a href="<?php echo get_permalink( get_page_by_path('vie-privee') ); ?>">
+            Vie privée
+        </a>
+
+        <p class="footer-text">Tous droits réservés</p>
+
+    </div>
+
 </footer>
 
-<?php wp_footer(); ?> 
+<?php wp_footer(); ?>
+
 </body>
 </html>
