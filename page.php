76bcb2c16709dcd25069ea7084ff27b43a0ca68e
Meli-Projets <melisande77320@gmail.com>
2026-07-01 10:40:11 +0200

    Make page and single templates editable

--- page.php
+++ page.php
@@ -1,7 +1,19 @@
 <?php get_header(); ?>
 
-<main>
-    <h1>Page</h1>
+<main class="page-content">
+
+    <?php
+    // WordPress Loop for page content
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+            //Content manageable via the WordPress editor
+            the_content();
+
+        endwhile;
+    endif;
+    ?>
+
 </main>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,7 +1,19 @@
 <?php get_header(); ?>
 
-<main>
-    <h1>Articles</h1>
+<main class="single-content">
+
+    <?php
+     // WordPress Loop for single page
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+            //Content manageable via the WordPress editor
+            the_content();
+
+        endwhile;
+    endif;
+    ?>
+
 </main>
 
 <?php get_footer(); ?>
